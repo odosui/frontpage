2158229247ef0328d85aab9afb6e67a82ab214ab
Ianis Triandafilov <janis.sci@gmail.com>
2026-03-13 17:10:45 +0100

    cleanup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 import bodyParser from "body-parser";
 import express, { Express, Request, Response } from "express";
-import multer from "multer";
 import os from "os";
 import path from "path";
 import { createApi } from "./api/api";
 import { createRoutes } from "./api/routes";
-import { createFSNotesStore } from "./components/notes/FSNotesStore";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 const PORT = process.env.MT_PORT || 3000;
@@ -30,22 +28,20 @@ export async function startServer(mtHomeArg: string) {
   app.use("/media", express.static(mediaPath));
 
   // init out app
-  const noteStore = await createFSNotesStore(mtHome);
-  const routes = createRoutes(createApi(noteStore, mtHome));
+  const routes = createRoutes(createApi());
 
-  // mapping apis
-  const upload = multer({ storage: multer.memoryStorage() });
+  //   // mapping apis
+  //   const upload = multer({ storage: multer.memoryStorage() });
 
   for (const m of routes) {
     const method = m.method.toLowerCase() as keyof Express;
-    const middlewares = m.multipart ? [upload.single("image")] : [];
+    // const middlewares = m.multipart ? [upload.single("image")] : [];
 
-    app[method](m.path, ...middlewares, async (req: Request, res: Response) => {
+    app[method](m.path, async (req: Request, res: Response) => {
       const { status, json } = await m.handler({
         pathParams: req.params as Record<string, string>,
         query: req.query as Record<string, string>,
         body: req.body,
-        ...(req.file ? { file: req.file } : {}),
       });
       res.status(status).json(json);
     });
